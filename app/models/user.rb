c167ea35d7eabc9c9a1daac20aa3607e2fd38898
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-06-25 23:13:30 +0800

    Added simple authentication using bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
+
+  validates :name, presence: true
+  validates :username, length: { in: 4..30 }, presence: true
+  validates :password_digest, length: { minimum: 8 }, presence: true
+  validates :email, presence: true
+
+  has_secure_password
+  # attr_reader :password
+  # validates_presence_of :password, on: :create
+  # validates_confirmation_of :password
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,9 @@ ActiveRecord::Schema.define(version: 2018_06_21_234341) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.string "username", null: false
     t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
