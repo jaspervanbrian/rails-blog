a3b5e808795f220236e306edfca7abe67ac28c26
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-07-16 15:58:28 +0800

    Add restriction to show conversation (if user is not in conversation, flash error then redirect.)

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,8 +6,8 @@ class ConversationsController < ApplicationController
     if params[:to_id].present?
       @user = User.find_by(id: params[:to_id])
       unless @user.nil?
-        @conversation = @user.id === session[:user_id] ? get_self_conversation
-                                                      : get_single_conversation
+        @conversation = helpers.self_send? ? get_or_new_self_conversation
+                                            : get_or_new_single_conversation
         unless @conversation.new_record?
           redirect_to conversation_path(@conversation)
         end
@@ -21,9 +21,16 @@ class ConversationsController < ApplicationController
   def create
     if params[:to_id].present? && message_params.present?
       @user = User.find_by(id: params[:to_id])
-      @conversation = get_single_conversation
-      @conversation.type = @user.id === session[:user_id] ? "SelfConversation"
-                                                          : "SingleConversation"
+      if helpers.self_send?
+        @conversation = get_or_new_self_conversation
+        redirect_to conversation_path(@conversation) if @conversation.persisted?
+        @conversation.type = "SelfConversation"
+      else
+        @conversation = get_or_new_single_conversation
+        redirect_to conversation_path(@conversation) if @conversation.persisted?
+        @conversation.type = "SingleConversation"
+      end
+
       @conversation.save
       ConversationsUser.create(conversation: @conversation, user: helpers.current_user)
       @conversation.users << @user if @conversation.type === "SingleConversation"
@@ -41,7 +48,7 @@ class ConversationsController < ApplicationController
   def show
     @conversations = helpers.current_user.conversations.latest
     @conversation = Conversation.find_by(id: params[:id])
-    if @conversation.nil?
+    if !@conversation.users.include?(helpers.current_user) || @conversation.nil?
       flash[:error] = "Conversation does not exist."
       redirect_to conversations_path
     end
@@ -59,12 +66,12 @@ class ConversationsController < ApplicationController
 
   private
 
-  def get_single_conversation
-    ConversationsUsersRepository.new.get_single_conversation(@user, session[:user_id]) # repositories/conversations_users_repositories
+  def get_or_new_single_conversation
+    ConversationsUsersRepository.new.get_or_new_single_conversation(@user, session[:user_id]) # repositories/conversations_users_repositories
   end
 
-  def get_self_conversation
-    ConversationsUsersRepository.new.get_self_conversation(session[:user_id])
+  def get_or_new_self_conversation
+    ConversationsUsersRepository.new.get_or_new_self_conversation(session[:user_id])
   end
 
   def message_params
--- app/repositories/conversations_users_repository.rb
+++ app/repositories/conversations_users_repository.rb
@@ -1,5 +1,5 @@
 class ConversationsUsersRepository
-  def get_single_conversation(user, my_id)
+  def get_or_new_single_conversation(user, my_id)
     conversation_id = ConversationsUser.where(user_id: [user.id, my_id])
                       .group("conversation_id")
                       .having("count(user_id) = 2")
@@ -8,11 +8,11 @@ class ConversationsUsersRepository
     Conversation.find_by(id: conversation_id) || Conversation.new
   end
 
-  def get_self_conversation(my_id)
+  def get_or_new_self_conversation(my_id)
     conversation = Conversation.where(type: "SelfConversation")
                     .left_joins(:conversations_users)
                     .where("conversations_users.user_id  = ?", my_id)
                     .first
-    conversation
+    conversation || Conversation.new
   end
 end
