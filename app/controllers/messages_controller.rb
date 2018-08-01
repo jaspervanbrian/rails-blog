73962cba2f7c5b627279ae368fe4160564cffeaf
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-08-01 19:43:20 +0800

    WIP ActionCable broadcast to users of a conversation list if a message to the conversation is sent.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,13 @@ class MessagesController < ApplicationController
         from: @message.user.id
       )
 
+      conversation.user_ids.each do |user_id|
+        ActionCable.server.broadcast("conversations_#{user_id}",
+          messages: [render_my_message, render_message],
+          from: @message.user.id
+        )
+      end
+
       head :ok
     end
   end
@@ -20,6 +27,15 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body, attachments: [])
+  end
+
+  def render_conversation_partial(conversation)
+    render_to_string(
+      partial: "partials/conversation",
+      locals: {
+        conversation: conversation
+      }
+    )
   end
 
   def render_my_message
