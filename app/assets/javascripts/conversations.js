173bb60958881986ed3bac890ba8211b0292f929
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-08-12 04:27:09 +0800

    Finish setting timestamps for existing convs.
    Timestamps are now self updating every minute. Used for-loop
    for better performance. Also, using self-executing anonymous
    functions to make the update happen to individual timestamps,
    and I think it makes the code performant IMO.
    
    Ref: https://stackoverflow.com/questions/7749090/how-to-use-setinterval-function-within-for-loop

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -3,4 +3,26 @@ $(document).on("turbolinks:load", function() {
 		var chatbox = $("#conversation").children(".card-body");
 		chatbox.scrollTop(chatbox[0].scrollHeight);
 	}
+	var conversations = $("#conversations");
+	if(conversations.find(".timestamp").length) {
+		var timestamps = conversations.find(".timestamp");
+		for(var i = 0; i < timestamps.length; i++) {
+			actual_timestamp = $(timestamps[i]);
+			last_message_date = moment(actual_timestamp.data('timestamp'), "YYYY-MM-DD HH:mm:ss");
+
+	    set_actual_timestamp(actual_timestamp, last_message_date);
+			(function(actual_timestamp, last_message_date) {
+		    setInterval(function() {
+	      	set_actual_timestamp(actual_timestamp, last_message_date);
+				}, 60000);
+			})(actual_timestamp, last_message_date);
+		}
+	}
 });
+
+function set_actual_timestamp(actual_timestamp, last_message_date) {
+  if(moment().isBefore(last_message_date, 'year'))
+    actual_timestamp.text(last_message_date.month() + "/" + last_message_date.date() + "/" + last_message_date.year())
+  else
+ 		actual_timestamp.text(last_message_date.fromNow())
+}
