24c9b0aecc1c87b3e18ebb229677ca7a764523d5
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-08-17 19:32:48 +0800

    Add timestamp segment on client side on channel
    Checks if the previous and current message have 2
    hours difference. It behaves like timestamp segm-
    ent in existing message. Because we have no way
    to put it realtime, this check and insert is hap-
    pening on the client side on channel.
    
    Data timestamp attribute also added on messages
    to retrieve raw timestamps.

--- app/assets/javascripts/channels/conversation.js
+++ app/assets/javascripts/channels/conversation.js
@@ -19,18 +19,18 @@ $(document).on("turbolinks:load", function() {
       received: function(data) {
         if(data['messages'].length !== 0) {
           // Called when there's incoming data on the websocket for this channel
+          var message = null;
         	conversation_box = $(conversation.children('.card-body').children('.col-12')[0]);
           var previous_message = conversation_box.children('.row').last();
-          var previous_sender = previous_message.children('span').first().data('sender');
           if(data['from'] == conversation.data("me")) {
-            var message = $(data['messages'][0]);
+            message = $(data['messages'][0]);
           	$("input#message_attachments").val(null);
             $("label.custom-file-label").text("No attachment selected.");
           	$("textarea").val("");
           } else {
-            var message = $(data['messages'][1]);
+            message = $(data['messages'][1]);
           }
-          insertMessage(conversation_box, message, previous_sender);
+          insertMessage(conversation_box, message, previous_message);
         }
         conversation_box = conversation.children('.card-body');
         conversation_box.scrollTop(conversation_box[0].scrollHeight);
@@ -39,14 +39,35 @@ $(document).on("turbolinks:load", function() {
   }
 });
 
-function insertMessage(conversation_box, message, previous_sender) {
-  if(message.find('span').first().data('sender') == previous_sender){
-    conversation_box.append(message.closest('.old'));
-    message.closest('.old').find('[data-toggle="tooltip"]').tooltip();
-    message.closest('.new').remove();
-  } else {
+function insertMessage(conversation_box, message, previous_message) {
+  var previous_message_timestamp = moment(previous_message.find('[data-timestamp]').data("timestamp"), "YYYY-MM-DD HH:mm:ss");
+  var message_timestamp = moment(message.find('[data-timestamp]').data("timestamp"), "YYYY-MM-DD HH:mm:ss");
+  var previous_sender = previous_message.children('span').first().data('sender');
+  console.log(previous_message_timestamp)
+  console.log(message_timestamp)
+  console.log((message_timestamp.diff(previous_message_timestamp, 'hours')));
+  if((message_timestamp.diff(previous_message_timestamp, 'hours')) >= 2) {
+    conversation_box.append(`
+      <div class="row">
+        <div class="col-12 text-center">
+          <small class="text-muted">
+            ${message_timestamp.format("MMMM DD, YYYY | h:mm A (dddd)")}
+          </small>
+        </div>
+      </div>
+    `);
     conversation_box.append(message.closest('.new'));
     message.closest('.new').find('[data-toggle="tooltip"]').tooltip();
     message.closest('.old').remove();
+  } else {
+    if(message.find('span').first().data('sender') == previous_sender){
+      conversation_box.append(message.closest('.old'));
+      message.closest('.old').find('[data-toggle="tooltip"]').tooltip();
+      message.closest('.new').remove();
+    } else {
+      conversation_box.append(message.closest('.new'));
+      message.closest('.new').find('[data-toggle="tooltip"]').tooltip();
+      message.closest('.old').remove();
+    }
   }
 }
